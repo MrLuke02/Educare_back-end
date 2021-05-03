70e9e5fa231aa326360b7906361b6aae8f9cda50
Thiago Rodrigues <thi013022001@gmail.com>
2021-05-03 19:26:20 -0300

    [tk-4] Improved token verification

--- src/auth/token.auth.ts
+++ src/auth/token.auth.ts
@@ -2,9 +2,9 @@ import { Response } from "express";
 import JWT from "jsonwebtoken";
 
 // criando o metodo de geração do token, com retorno em forma de promise
-const generate = (payload: Object, res: Response) =>
-  new Promise((resolve) => {
-    // chamando o metodo sign do JWT que é o metodo que de fato cria o token
+const createToken = (payload: Object, res: Response) => {
+  return new Promise((resolve) => {
+    // chamando o metodo sign do JWT responsável por criar o token
     JWT.sign(
       // conteúdo do token
       payload,
@@ -15,7 +15,7 @@ const generate = (payload: Object, res: Response) =>
         algorithm: "HS256",
         expiresIn: "1h",
       },
-      // a função para retornar o token caso ocorra tudo bem, caso de algo errado retorna um json de error
+      // função para retornar o token caso ocorra tudo bem, caso de algo errado retorna um json de error
       function (err, token) {
         if (err) {
           return res.status(400).json({ error: "Token inválido!" });
@@ -24,6 +24,31 @@ const generate = (payload: Object, res: Response) =>
       }
     );
   });
+};
 
-// exportando a funcao
-export { generate };
+// criando o metodo de verificação do token, com retorno em forma de promise
+const verifyToken = (token: string, res: Response) => {
+  return new Promise((resolve) => {
+    // chamando o metodo verify do JWT responsável por verificar o token
+    JWT.verify(
+      // token a ser verificado
+      token,
+      // chave secreta
+      process.env.SECRET_KEY,
+      // algoritmo que foi usado para criptografar o token
+      {
+        algorithms: ["HS256"],
+      },
+      // função para retornar o token caso ocorra tudo bem, caso de algo errado retorna um json de error
+      function (err, token) {
+        if (err) {
+          return res.status(400).json({ error: "Sessão expirada!" });
+        }
+        resolve(token);
+      }
+    );
+  });
+};
+
+// exportando as funcões
+export { createToken, verifyToken };
