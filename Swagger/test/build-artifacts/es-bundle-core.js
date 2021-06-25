2ae2bd72193898d9e0831a03f13f30691d1cdcbb
Weliton vitor Alves <wellingtonvictoralves@gmail.com>
2021-06-25 17:57:57 -0300

    [tk-26] Documentation API Change in role routes

--- Swagger/test/build-artifacts/es-bundle-core.js
+++
@@ -1,7 +0,0 @@
-import SwaggerEditor from '../../dist/swagger-editor-es-bundle-core';
-
-describe('webpack browser es-bundle build', () => {
-  test('should export a function for es-bundle', () => {
-    expect(SwaggerEditor).toBeInstanceOf(Function);
-  });
-});
--- Swagger/test/build-artifacts/es-bundle.js
+++
@@ -1,7 +0,0 @@
-import SwaggerEditor from '../../dist/swagger-editor-es-bundle';
-
-describe('webpack browser es-bundle build', () => {
-  test('should export a function for es-bundle', () => {
-    expect(SwaggerEditor).toBeInstanceOf(Function);
-  });
-});
